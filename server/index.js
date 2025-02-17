dfc55af930ad0ef44bab7cd558aa79649b3658e1
noppharat2499 <noparat.d@ku.th>
2025-02-17 13:59:32 +0700

    Update mySQL

--- server/index.js
+++ server/index.js
@@ -16,13 +16,37 @@ server.listen(port, host, () => {
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mysql = require('mysql2/promise');
 const app = express();
 const port = 8000;
 
 app.use(bodyParser.json());
 
 let users = []
-let counter = 1
+
+let conn = null
+const initMySQL = async () => {
+  conn = await mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'webdb',
+    port: 8820
+  })
+}
+
+/*
+app.get('/testdbnew', async (req, res) => {
+
+  try {
+      const result = await conn.query('SELECT * FROM users')
+      res.json(result[0])
+  } catch (error) {
+      console.log('error', error.message)
+      res.status(500).json({error: 'Error fetching users'})
+  }
+})
+*/
 
 /*
 GET /users สำหรับ get users ทั้งหมดที่บันทึกไว้
@@ -33,20 +57,28 @@ DELETE /users/:id สำหรับลบ users รายคน (ตาม id �
 */
 
 //path: GET /users สำหรับแสดงข้อมูล user ทั้งหมดที่บันทึกไว้
-app.get('/users', (req, res) => {
-  res.json(users);
+app.get('/users', async (req, res) => {
+  const result = await conn.query('SELECT * FROM users')
+  res.json(result[0])
 })
 
 //path: POST /users ใช้สำหรับสร้างข้อมูล user ใหม่บันทึกเข้าไป
-app.post('/user', (req, res) => {
+app.post('/users', async (req, res) => {
   let user = req.body;
-  user.id = counter
-  counter += 1
-  users.push(user);
+  const result = await conn.query('INSERT INTO users SET ?', user)
+  console.log('result', result)
   res.json({
-    message: 'Create new user successfully.',
-    user: user
-  });
+    message: 'Create user successfully',
+    data: result[0]
+  })
+})
+
+//path: GET /users/:id สำหรับดึง user รายคนออกมา
+app.get('/users/:id', (req, res) => {
+  let id = req.params.id;
+  // ค้นหา user หรือ index ที่ต้องการดึงข้อมูล
+  let selectedIndex = users.findIndex(user => user.id == id)
+  res.json(users[selectedIndex])
 })
 
 //path: PUT /user/:id ใช้สำหรับแก้ไขข้อมูล user โดยใช้ id เป็นตัวระบุ
@@ -57,13 +89,10 @@ app.put('/user/:id', (req, res) => {
   // ค้นหา user ที่ต้องการแก้ไข
   let selectedIndex = users.findIndex(user => user.id == id )
 
-  // แก้ไขข้อมูล user ที่หาเจอ
-  if (updateUser.firstname) {
-    users[selectedIndex].firstname = updateUser.firstname
-  }
-  if (updateUser.lastname) {
-    users[selectedIndex].lastname = updateUser.lastname
-  }
+  users[selectedIndex].firstname = updateUser.firstname || users[selectedIndex].firstname
+  users[selectedIndex].lastname = updateUser.lastname || users[selectedIndex].lastname
+  users[selectedIndex].age = updateUser.age || users[selectedIndex].age
+  users[selectedIndex].gender = updateUser.gender || users[selectedIndex].gender
 
   res.json({
     message: 'Update user successfully.',
@@ -89,6 +118,7 @@ app.delete('/user/:id', (req, res) => {
   })
 })
 
-app.listen(port, (req, res) => {
+app.listen(port, async (req, res) => {
+  await initMySQL()
   console.log('Http Server is running on port' + port);
 });
